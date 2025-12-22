34a6d0a20092fdc7460abcc857d58c5df4f814f3
vodobryshkin <dobryshkinvladimir@yandex.ru>
2025-12-22 23:43:09 +0300

    fixed email validator

--- src/main/java/com/vdska/pointsapi2/validaton/formats/email/EmailValidator.java
+++ src/main/java/com/vdska/pointsapi2/validaton/formats/email/EmailValidator.java
@@ -1,6 +1,5 @@
 package com.vdska.pointsapi2.validaton.formats.email;
 
-import com.vdska.pointsapi2.validaton.formats.email.Email;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.stereotype.Component;
@@ -13,6 +12,6 @@ public class EmailValidator implements ConstraintValidator<Email, String> {
     @Override
     public boolean isValid(String name, ConstraintValidatorContext context) {
         if (name == null || name.isBlank()) return true;
-        return name.matches("^\\w{8,32}$");
+        return name.matches("^\\w+@\\w+\\.\\w{2}$");
     }
 }
