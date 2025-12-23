d8d6287f8509af6a8c374017b0e7f80fa9d9cfa4
vodobryshkin <dobryshkinvladimir@yandex.ru>
2025-12-23 22:38:02 +0300

    added RabitMQ into the project for the mail queue

--- src/main/java/com/vdska/pointsapi2/Application.java
+++ src/main/java/com/vdska/pointsapi2/Application.java
@@ -1,10 +1,22 @@
 package com.vdska.pointsapi2;
 
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AllArgsConstructor
 public class Application {
+    private RabbitAdmin rabbitAdmin;
+    private Queue queue;
+
+    @PostConstruct
+    public void init() {
+        rabbitAdmin.declareQueue(queue);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ src/main/java/com/vdska/pointsapi2/config/RabbitMqConfig.java
@@ -0,0 +1,46 @@
+package com.vdska.pointsapi2.config;
+
+import lombok.Setter;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Конфигурация для корректной работы очереди сообщений на основе Rabbit MQ
+ */
+@Configuration
+@Setter
+public class RabbitMqConfig {
+    @Value("${mail.queue.name}")
+    private String queueName;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName);
+    }
+
+    @Bean
+    public CachingConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
+}
