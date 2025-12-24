e4da0d37d9d813ce148d4c4d27b91b3c3b82490b
vodobryshkin <dobryshkinvladimir@yandex.ru>
2025-12-24 17:46:57 +0300

    added SecurityConfig and JwtAuthenticationFilter for JWT authentication + authorization

--- src/main/java/com/vdska/pointsapi2/config/SecurityConfig.java
+++ src/main/java/com/vdska/pointsapi2/config/SecurityConfig.java
@@ -1,61 +1,58 @@
 package com.vdska.pointsapi2.config;
 
+import com.vdska.pointsapi2.security.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.sql.DataSource;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-//        http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .cors(cors -> {})
-//                .sessionManagement(session ->
-//                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                )
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/v1/auth/**").permitAll()
-//                        .anyRequest().authenticated()
-//                );
-//
-//        return http.build();
-//    }
-//    @Bean
-//    public UserDetailsService userDetailsService(DataSource dataSource) {
-//        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
-//
-//        userDetailsManager.setUsersByUsernameQuery(
-//                "select username, password, enabled " +
-//                        "from users where username = ?"
-//        );
-//
-//        userDetailsManager.setAuthoritiesByUsernameQuery(
-//                "select username, authority " +
-//                        "from authorities where username = ?"
-//        );
-//
-//        return userDetailsManager;
-//    }
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> {})
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll()
-                );
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        userDetailsManager.setUsersByUsernameQuery(
+                "select username, password, enabled " +
+                        "from users where username = ?"
+        );
+
+        userDetailsManager.setAuthoritiesByUsernameQuery(
+                "select username, authority " +
+                        "from authorities where username = ?"
+        );
+
+        return userDetailsManager;
     }
 
     @Bean
