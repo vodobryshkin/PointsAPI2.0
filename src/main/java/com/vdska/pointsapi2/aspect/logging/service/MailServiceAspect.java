e9eb67ad512b34a06c398f36e3098fa83e5ddd2b
vodobryshkin <dobryshkinvladimir@yandex.ru>
2025-12-25 00:21:04 +0300

    added logging for new methods in mail service and message service

--- src/main/java/com/vdska/pointsapi2/aspect/logging/service/MailServiceAspect.java
+++ src/main/java/com/vdska/pointsapi2/aspect/logging/service/MailServiceAspect.java
@@ -1,6 +1,7 @@
 package com.vdska.pointsapi2.aspect.logging.service;
 
 import com.vdska.pointsapi2.dto.mail.ConfirmAccountMailRequest;
+import com.vdska.pointsapi2.dto.mail.OTPMailRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -15,6 +16,9 @@ public class MailServiceAspect {
     @Pointcut("execution(public * com.vdska.pointsapi2.service.spec.IMailService.sendConfirmationLetter(..))")
     public void sendConfirmationLetterPointcutMethod(){}
 
+    @Pointcut("execution(public * com.vdska.pointsapi2.service.spec.IMailService.sendOTPLetter(..))")
+    public void sendOTPLetterPointcutMethod(){}
+
     @Around("sendConfirmationLetterPointcutMethod()")
     public Object logSendConfirmationLetter(ProceedingJoinPoint pjp) throws Throwable {
         Object result;
@@ -25,6 +29,19 @@ public class MailServiceAspect {
         String email = request.getTo();
 
         log.debug("Письмо с ссылкой на подтверждение аккаунта для пользователя с username='{}' было отправлено на email {}.", username, email);
+        return result;
+    }
+
+    @Around("sendOTPLetterPointcutMethod()")
+    public Object logSendOTPLetter(ProceedingJoinPoint pjp) throws Throwable {
+        Object result;
+        result = pjp.proceed();
+        OTPMailRequest request = (OTPMailRequest) pjp.getArgs()[0];
+
+        String username = request.getUsername();
+        String email = request.getTo();
+
+        log.debug("Письмо с OTP для пользователя с username='{}' было отправлено на email {}.", username, email);
         return result;
     }
 
--- src/main/java/com/vdska/pointsapi2/aspect/logging/service/MessageServiceAspect.java
+++ src/main/java/com/vdska/pointsapi2/aspect/logging/service/MessageServiceAspect.java
@@ -1,6 +1,7 @@
 package com.vdska.pointsapi2.aspect.logging.service;
 
 import com.vdska.pointsapi2.dto.mail.ConfirmAccountMailRequest;
+import com.vdska.pointsapi2.dto.mail.OTPMailRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -15,6 +16,9 @@ public class MessageServiceAspect {
     @Pointcut("execution(public * com.vdska.pointsapi2.service.spec.IMessageService.sendConfirmAccountMessageToQueue())")
     public void sendConfirmAccountMessageToQueuePointcutMethod(){}
 
+    @Pointcut("execution(public * com.vdska.pointsapi2.service.spec.IMessageService.sendOTPMessageToQueue())")
+    public void sendOTPMessageToQueuePointcutMethod(){}
+
     @Around("sendConfirmAccountMessageToQueuePointcutMethod()")
     public Object logSendConfirmAccountMessageToQueuePointcutMethod(ProceedingJoinPoint pjp) throws Throwable{
         Object result;
@@ -25,6 +29,19 @@ public class MessageServiceAspect {
         String email = request.getTo();
 
         log.debug("Данные для письма на подтверждение аккаунта для пользователя с username='{}' и email='{}' были отправлены в очередь сообщений.", username, email);
+        return result;
+    }
+
+    @Around("sendOTPMessageToQueuePointcutMethod()")
+    public Object logSendOTPMessageToQueuePointcutMethod(ProceedingJoinPoint pjp) throws Throwable{
+        Object result;
+        result = pjp.proceed();
+        OTPMailRequest request = (OTPMailRequest) pjp.getArgs()[0];
+
+        String username = request.getUsername();
+        String email = request.getTo();
+
+        log.debug("Данные для письма с OTP для пользователя с username='{}' и email='{}' были отправлены в очередь сообщений.", username, email);
         return result;
     }
 }
